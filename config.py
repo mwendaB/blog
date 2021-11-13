a0e9cc715bbe36f794ee1d370b0ad6e4f6ff3b86
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 13:44:11 +0300

    write the manage app

--- config.py
+++ config.py
@@ -1,20 +1,35 @@
 import os
-import cloudinary
+from dotenv import load_dotenv
+load_dotenv()
 class Config:
-
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringaschool:mwendaB@localhost/blog'
-
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-
-    #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'MWENDA Blog!'
-    SENDER_EMAIL = 'brianmwenda255@gmail.com'
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    DEBUG = True
 
-        
+
+    
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringaschool:mwendaB@localhost/blog'
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
+
+
+
