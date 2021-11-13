b3dd37326d76128c68bacf714ce5c864d4cde02c
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 13:49:48 +0300

    write the db class

--- app/models.py
+++ app/models.py
@@ -2,3 +2,34 @@ from . import db,login_manager
 from flask_login import current_user,UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+class User (UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email = db.Column(db.String(255), unique = True,nullable = False)
+    bio = db.Column(db.String(255),default ='My default Bio')
+    profile_pic_path = db.Column(db.String(150),default ='default.png')
+    hashed_password = db.Column(db.String(255),nullable = False)
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+
+
+
+    @property
+    def set_password(self):
+        raise AttributeError('Password attribute cannot be read')
+
+    @set_password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.hashed_password,password)
