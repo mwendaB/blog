47a19b210f407be02d64f9a6157a2e0da579fdf5
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 18:23:46 +0300

    reinstall flask

--- manage.py
+++ manage.py
@@ -1,29 +1,29 @@
-from flask_script import Manager,Server
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import User, Role, Post, Comment, Category, Subscriber
 from flask_migrate import Migrate, MigrateCommand
-from app import create_app,db
-from app.models import User
+# Creating app instance
+app = create_app('production')
 
+manager = Manager(app)
+migrate = Migrate(app, db)
 
-app = create_app ('development')
-
-
-manager =  Manager(app)
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
 manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
 
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db,User = User)
 
 @manager.command
 def test():
-    '''
-    Run the unit tests
-    '''
+    """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=5).run(tests)
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role, Post=Post, Comment=Comment, Category=Category, Subscriber=Subscriber)
+
 
 if __name__ == '__main__':
     manager.run()
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY=mwendaB
+export MAIL_USERNAME=brianmwendah12@gmail.com
+export MAIL_PASSWORD=bryangarizo@gmail.com
+
+
+python3 manage.py server
