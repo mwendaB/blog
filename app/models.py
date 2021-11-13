a4cc489d78264949be3815d8d179f0875c7250d5
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 13:56:37 +0300

    create subscribers class

--- app/models.py
+++ app/models.py
@@ -97,3 +97,7 @@ class Comment(db.Model):
     def __repr__(self):
         return f'Comment {self.comment}'
 
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
