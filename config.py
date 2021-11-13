31662142c7f49a23c1a08cadcc7983ac3730520c
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 23:01:32 +0300

    debug

--- config.py
+++ config.py
@@ -1,32 +1,34 @@
-
-
-import os
+import os 
 
 class Config:
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:mwendaB@localhost/blog'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringaschool:mwendaB@localhost/blog"
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
 
-    QUOTE_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
-    QUOTE_API_KEY = os.environ.get('QUOTE_API_KEY')
-
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    CSRF_ENABLED = True
-
-
-
-
-
+    # email configurations
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
+    
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+class TestConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringaschool:mwendaB@localhost/blog"
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:mwendaB@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringaschool:mwendaB@localhost/blog"
     DEBUG = True
 
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    "development": DevConfig,
+    "production": ProdConfig,
+    "test": TestConfig
 }
+
+
--- manage.py
+++ manage.py
@@ -1,20 +1,32 @@
-from app import create_app,db
-from flask_script import Manager,Server
-from app.models import User,BlogPost,Quote
-from  flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app.models import User, Post, Comment, PostLike
 
-# Creating app instance
-app = create_app('development')
-
+app = create_app("production")
 manager = Manager(app)
+manager.add_command("server", Server)
 
-manager.add_command('server',Server)
+migrate = Migrate(app, db)
+manager.add_command("db",MigrateCommand)
 
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+@manager.command
+def test():
+    """
+    Run the unittests
+    """
+    import unittest
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity = 2).run(tests)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,BlogPost = BlogPost,Quote = Quote)
+    return dict(app = app,
+                db = db,
+                User = User, 
+                Post = Post,
+                Comment = Comment,
+                PostLike = PostLike)
+
 if __name__ == '__main__':
     manager.run()
