08e957d594816237d009390bebc8e4e60c88a84b
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 13:58:45 +0300

    remove un used import

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from flask_sqlalchemy.model import camel_to_snake_case
 from . import db,login_manager
 from flask_login import current_user,UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
