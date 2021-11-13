7cc6e43c4da0e3e6c9eb8e4f8e90c2919fc0f5fa
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 14:29:40 +0300

    finish writing the requests and add the json link

---
+++ app/requests.py
@@ -0,0 +1,7 @@
+from app import  requests
+
+def get_quotes():
+    response = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
+    if response.status_code == 200:
+        quote = response.json()
+        return quote
