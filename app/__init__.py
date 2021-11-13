70e4999fba1e78adcd202f9fa0d97b56c2eb069b
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 13:33:03 +0300

    add the db sequence

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,11 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import IMAGES, UploadSet, configure_uploads
+
+db = SQLAlchemy()
+bootstap = Bootstrap()
+mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
