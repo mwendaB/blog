9113b99df6f142fbaff0fe25c149bc59f61d17b2
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 13:52:09 +0300

    write the blog class

--- app/models.py
+++ app/models.py
@@ -40,6 +40,15 @@ class User (UserMixin,db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-        
+
     def __repr__(self):
         return "User: %s" %str(self.username)
+
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(255),nullable=False)
+    content = db.Column(db.Text(),nullable=False)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship('Comment', backref='blog', lazy='dynamic')
