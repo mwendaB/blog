0f1c8a494e6ba46cd434cca81539e21c362ed8de
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-13 14:07:10 +0300

    write the sequnce

--- app/email.py
+++ app/email.py
@@ -3,3 +3,8 @@ from . import mail
 
 def mail_message(subject,template,to,**kwargs):
       sender_email = 'brianmwendah12@gmail.com'
+
+      email = Message(subject, sender=sender_email, recipients=[to])
+      email.body= render_template(template + ".txt",**kwargs)
+      email.html = render_template(template + ".html",**kwargs)
+      mail.send(email)
