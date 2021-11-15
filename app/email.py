01df2d04371977c3a41eaf5c7f8c564ccda2c781
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-15 12:02:51 +0300

    deploy to heroku

--- app/email.py
+++ app/email.py
@@ -3,7 +3,7 @@ from . import mail
 from flask import render_template
 
 def mail_message(subject,template,to,**kwargs):
-    sender_email = 'brianmwenda12@gmail.com'
+    sender_email = 'brianmwenda8193@gmail.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 
 app=create_app('test')
-app = create_app('production')
+app = create_app('development')
 
 
 manager =  Manager(app)
